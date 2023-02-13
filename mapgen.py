135d01a296a7258abf81d01a81ea9a1454dea893
R0X0RE0 <kawaiicucumber572@gmail.com>
2023-02-13 14:21:02 -0500

    what am I doing

--- mapgen.py
+++ mapgen.py
@@ -1,24 +1,28 @@
 from classes import *
+from defaults import *
 def genMap(ground, wall):
+    
+    grounds.clear()
+    walls.clear()
     x = 0
     y= 0
     map_elements = ["#", "_"]
     map1 = []
     map_row = ""
-    for i in range(12):
-        if (i == 0) or (i == 11):
-            for j in range(12): map_row += map_elements[0]
+    for i in range(MAP_SIZE):
+        if (i == 0) or (i == MAP_SIZE - 1):
+            for j in range(MAP_SIZE): map_row += map_elements[0]
         else:
-            for j in range(12):
-                if (j == 0) or (j == 11): map_row += map_elements[0]
-                elif (j == 11) : map_row += map_elements[1]
-                elif map_row.find("#_"): map_row += "_"
+            for j in range(MAP_SIZE):
+                if (j == 0) or (j == MAP_SIZE - 1): map_row += map_elements[0]
+                elif (j == 1) and (i == 1): map_row += map_elements[1]
+                elif (i > 1) and (map1[i-1][j]==map_elements[0]) : map_row += map_elements[1]
+                elif (len(map1) == MAP_SIZE) and (j > 1) and (map1[i][j-1]==map_elements[1]) : map_row += map_elements[0]
                 else : map_row += map_elements[random.randint(0, 1)]
         map1.append(map_row)
         map_row = ""
     for i in range(len(map1)):
         for j in range(len(map1[i])):
-            print(map1[i])
             if map1[i][j] == '_':
                 Floor(ground, x, y)
             elif map1[i][j] == '#':
